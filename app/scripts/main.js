e37d1cf64a9ec751eb105df0063c40a82bec7203
WordAppeal <cviale@pelhammedia.com>
2016-07-26 19:56:00 +0200

    work in progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+
+	// Grid - Home
+	var $grid = $(".grid").isotope({
+		itemSelector: ".grid .item",
+		layoutMode: "packery",
+		packery: {gutter: 15}
+	});
+
+	// Stories / About - Home
+	$(".hover").mouseleave(
+		function() {
+			$(this).removeClass("hover");
+		}
+	);
+});
