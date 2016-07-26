ffdb2e1e5609f8710e421256598547dabb89cb2b
WordAppeal <cviale@pelhammedia.com>
2016-07-26 20:27:05 +0200

    work in progress again !

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
 
 	// Grid - Home
-	var $grid = $(".grid").isotope({
-		itemSelector: ".grid .item",
-		layoutMode: "packery",
+	var $grid = $('.grid').isotope({
+		itemSelector: '.grid .item',
+		layoutMode: 'packery',
 		packery: {gutter: 15}
 	});
 
 	// Stories / About - Home
-	$(".hover").mouseleave(
+	$('.hover').mouseleave(
 		function() {
-			$(this).removeClass("hover");
+			$(this).removeClass('hover');
 		}
 	);
+
+	// Scroll mouse - Home
+	$(".mouse-icon").click(function() {
+		$("html, body").animate({ scrollTop: $(".boutique").offset().top - 0}, 1000);
+	});
+
 });
