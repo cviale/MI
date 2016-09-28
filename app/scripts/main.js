663513449f338160e90343fe2df2644ad3f60d0d
cviale <cviale@pelhammedia.com>
2016-09-28 08:18:07 +0200

    home

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
 
-	// Grid - Home
-	var $grid = $('.grid').isotope({
-		itemSelector: '.grid .item',
-		layoutMode: 'packery',
-		packery: {gutter: 15}
-	});
-
 	// Stories / About - Home
 	$('.hover').mouseleave(
 		function() {
