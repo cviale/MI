9900674f648b7610e554c4621fa8c21718b524d7
cviale <cviale@pelhammedia.com>
2016-10-16 15:44:04 +0200

    MI

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -104,4 +104,74 @@ $(document).ready(function() {
 		pauseOnHover: false
 	});
 
+	// Carousel - Single Shop
+	$('.slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 1000,
+		arrows: true,
+		fade: false,
+		asNavFor: '.slider-nav',
+		responsive: [
+			{
+				breakpoint: 770,
+				settings: {
+					arrows: true
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: true
+				}
+			}
+		]
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 1000,
+		asNavFor: '.slider-for',
+		dots: false,
+		arrows: false,
+		centerMode: true,
+		focusOnSelect: true
+	});
+
+	// Isotope - Cat Shop
+	var $grid = $('.grid').isotope({
+		layoutMode: 'packery',
+		itemSelector: '.item',
+		packery: {gutter: 30}
+	});
+
+	// Filtres - Cat Shop
+	var filters = {};
+	$('.filters').on( 'click', '.button', function() {
+		var $this = $(this);
+		var $buttonGroup = $this.parents('.button-group');
+		var filterGroup = $buttonGroup.attr('data-filter-group');
+		filters[ filterGroup ] = $this.attr('data-filter');
+		var filterValue = concatValues( filters );
+		$grid.isotope({ filter: filterValue });
+	});
+
+	// change is-checked class on buttons
+	$('.button-group').each( function( i, buttonGroup ) {
+		var $buttonGroup = $( buttonGroup );
+		$buttonGroup.on( 'click', 'button', function() {
+			$buttonGroup.find('.is-checked').removeClass('is-checked');
+			$( this ).addClass('is-checked');
+		});
+	});
+
+	// flatten object by concatting values
+	function concatValues( obj ) {
+		var value = '';
+		for ( var prop in obj ) {
+			value += obj[ prop ];
+		}
+		return value;
+	}
+
 });
